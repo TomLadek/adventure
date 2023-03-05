5b88a486f8dda97a27e145c47b4b5bf5899d9e02
Tom Ladek <ladek.tom@gmail.com>
2023-03-05 18:31:36 +0100

    clean up set ssr mode to ssg (prerender=true)

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,23 +1,19 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
 import ssr from 'vite-plugin-ssr/plugin'
 
-// Plugins
-import imageGenerator from "./src/rollup-plugin-image-generator.js";
-import dataManager from "./vite-plugin-data-manager.js";
-
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   const conf = {
+    base: "/adventure/test",
     define: {
       __VUE_I18N_FULL_INSTALL__: true,
       __VUE_I18N_LEGACY_API__: false,
       __INTLIFY_PROD_DEVTOOLS__: false,
     },
-    plugins: [vue(), ssr(), imageGenerator(), dataManager()],
+    plugins: [vue(), ssr({ prerender: true })],
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
