5b811c1ffa6b594367cad92270c5de339224d8ab
Tom Ladek <ladek.tom@gmail.com>
2023-03-26 18:42:56 +0200

    add content to index page add dummy popup for creating new adventures

--- app/pages/index/index.page.vue
+++ app/pages/index/index.page.vue
@@ -1,6 +1,107 @@
+<script>
+import { ref } from "vue";
+import CmsNewAdventurePopup from "../../src/components/CmsNewAdventurePopup.vue"
+</script>
+
 <script setup>
+const newAdventurePopupShowing = ref(false)
+let testAdventures = new Array(5)
 </script>
 
 <template>
-    <a href="/2023-myadventure">Adventure</a>
+  <main class="index">
+    <h1 class="cms-adventures-hdl">Adventures</h1>
+    <ul class="cms-adventure-list">
+      <li class="cms-adventure-list-item" v-for="(_) in testAdventures">
+        <a href="/2023-myadventure" class="cms-adventure-link">Adventure</a>
+      </li>
+      <li class="cms-adventure-list-item new-item">
+        <button class="cms-adventure-item-button-new" @click="newAdventurePopupShowing = true">
+          <svg width="32px" height="32px" class="new-item-icon">
+            <rect x="13" y="0" rx="2" ry="2" width="6" height="32" fill="#CCCCCC"/>
+            <rect x="0" y="13" rx="2" ry="2" width="32" height="6" fill="#CCCCCC"/>
+          </svg>
+        </button>
+      </li>
+    </ul>
+  </main>
+
+  <Transition name="popup-fade">
+      <CmsNewAdventurePopup v-if="newAdventurePopupShowing" @closing="newAdventurePopupShowing = false"></CmsNewAdventurePopup>
+    </Transition>
 </template>
+
+<style>
+:root {
+  --adventure-list-item-width: 20rem;
+  --adventure-list-item-height: 11.25rem;
+}
+
+main.index {
+  color: initial;
+  padding: 3rem;
+  position: relative;
+}
+
+.cms-adventures-hdl {
+  text-align: center;
+  margin-bottom: 4rem;
+}
+
+.cms-adventure-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  gap: 1rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, var(--adventure-list-item-width));
+  justify-content: center;
+}
+
+.cms-adventure-list-item {
+  width: var(--adventure-list-item-width);
+  height: var(--adventure-list-item-height);
+  background-color: rgb(206, 206, 206);
+  border-radius: 10px;
+}
+
+.cms-adventure-list-item.new-item {
+  background-color: white;
+  border: 2px dashed #CCCCCC;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.cms-adventure-item-button-new {
+  width: 100%;
+  height: 100%;
+  background: none;
+  padding: 0;
+  border: none;
+  cursor: pointer;
+}
+
+.new-item-icon {
+  border: 1px solid #CCCCCC;
+  border-radius: 50%;
+  padding: 10px;
+}
+
+.cms-adventure-link {
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+}
+
+.popup-fade-enter-active, .popup-fade-leave-active {
+  transition: all 0.2s ease;
+}
+
+.popup-fade-enter-from,
+.popup-fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ app/src/components/CmsNewAdventurePopup.vue
@@ -0,0 +1,73 @@
+<script>
+import { defineEmits, onMounted, onUnmounted } from "vue";
+</script>
+
+<script setup>
+const emit = defineEmits(["closing"]);
+
+function closePopup() {
+  emit("closing");
+}
+
+function onKeyUp(e) {
+  console.log("keyup!", e)
+  if (e.key === "Escape")
+    closePopup()
+}
+
+onMounted(() => {
+  addEventListener("keyup", onKeyUp);
+})
+
+onUnmounted(() => {
+  removeEventListener("keyup", onKeyUp);
+})
+</script>
+
+<template>
+<div class="cms-new-adventure-popup">
+  <div class="cms-new-adventure-popup-content">
+    <button class="popup-button-close" @click="closePopup">
+      <svg width="30" height="30" view-box="0 0 30 30" stroke="#787878" stroke-width="3px" stroke-linecap="round">
+        <line x1="10" y1="10" x2="20" y2="20"></line>
+        <line x1="10" y1="20" x2="20" y2="10"></line>
+      </svg>
+    </button>
+  </div>
+</div>
+</template>
+
+<style>
+.cms-new-adventure-popup {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #00000036;
+}
+
+.cms-new-adventure-popup-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: min(40rem, 80vw);
+  height: 30rem;
+  transform: translate(-50%, -50%);
+  border-radius: 32px;
+  background: #fff;
+  box-shadow: 0px 0px 32px 0px #686868;
+}
+
+.popup-button-close {
+  position: absolute;
+  top: -8px;
+  right: -8px;
+  width: 2rem;
+  height: 2rem;
+  padding: 0;
+  border: 1px solid #787878;
+  border-radius: 50%;
+  background: #fff;
+}
+</style>
