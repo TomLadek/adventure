1da6f3a43d9c3a6d0d4cfdb43435098132ce28a2
Tom Ladek <ladek.tom@gmail.com>
2023-03-28 11:55:10 +0200

    add inputs for new adventure popup improve new adventure popup style

---
+++ app/src/components/ButtonClose.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineEmits } from "vue";
+
+defineEmits(["close-click"]);
+</script>
+
+<template>
+  <button class="button-close" @click="this.$emit('close-click')">
+    <svg width="30" height="30" view-box="0 0 30 30" stroke="#787878" stroke-width="3px" stroke-linecap="round">
+      <line x1="10" y1="10" x2="20" y2="20"></line>
+      <line x1="10" y1="20" x2="20" y2="10"></line>
+    </svg>
+  </button>
+</template>
+
+<style>
+.button-close {
+  width: 2rem;
+  height: 2rem;
+  padding: 0;
+  border: 1px solid #787878;
+  border-radius: 50%;
+  background: #fff;
+  cursor: pointer;
+}
+</style>
--- app/src/components/CmsNewAdventurePopup.vue
+++ app/src/components/CmsNewAdventurePopup.vue
@@ -1,5 +1,7 @@
 <script>
-import { defineEmits, onMounted, onUnmounted } from "vue";
+import { defineEmits, onMounted, onUnmounted, ref } from "vue";
+
+// import ButtonClose from "./ButtonClose.vue";
 </script>
 
 <script setup>
@@ -9,10 +11,21 @@ function closePopup() {
   emit("closing");
 }
 
+function confirm() {
+  closePopup();
+}
+
 function onKeyUp(e) {
   if (e.key === "Escape")
     closePopup()
 }
+
+const newAdventureData = ref({
+  title: "",
+  urlPath: "",
+  author: "",
+  authorText: ""
+})
 
 onMounted(() => {
   addEventListener("keyup", onKeyUp);
@@ -26,12 +39,25 @@ onUnmounted(() => {
 <template>
 <div class="cms-new-adventure-popup">
   <div class="cms-new-adventure-popup-content">
-    <button class="popup-button-close" @click="closePopup">
-      <svg width="30" height="30" view-box="0 0 30 30" stroke="#787878" stroke-width="3px" stroke-linecap="round">
-        <line x1="10" y1="10" x2="20" y2="20"></line>
-        <line x1="10" y1="20" x2="20" y2="10"></line>
-      </svg>
-    </button>
+    <!-- <ButtonClose class="popup-button-close" @close-click="closePopup" /> -->
+
+    <div class="cms-new-adventure-fields-container">
+      <h2>New Adventure</h2>
+      <div class="cms-new-adventure-fields">
+          <label for="input-title">Title:</label>
+          <input type="text" id="input-title" v-model="newAdventureData.title">
+          <label for="input-url-path">URL path:</label>
+          <input type="text" id="input-url-path" v-model="newAdventureData.urlPath">
+          <label for="input-url-author">Author:</label>
+          <input type="text" id="input-url-author" v-model="newAdventureData.author">
+          <label for="input-url-author-text">Text author(s):</label>
+          <input type="text" id="input-url-author-text" v-model="newAdventureData.authorText">
+      </div>
+      <div class="cms-new-adventure-actions">
+        <button class="button-ok" @click="confirm">OK</button>
+        <button class="button-cancel" @click="closePopup">Cancel</button>
+      </div>
+    </div>
   </div>
 </div>
 </template>
@@ -50,24 +76,92 @@ onUnmounted(() => {
   position: absolute;
   top: 50%;
   left: 50%;
-  width: min(40rem, 80vw);
-  height: 30rem;
+  width: min(30rem, 80vw);
   transform: translate(-50%, -50%);
   border-radius: 32px;
   background: #fff;
   box-shadow: 0px 0px 32px 0px #686868;
 }
 
+/*
 .popup-button-close {
   position: absolute;
   top: -0.5rem;
   right: -0.5rem;
-  width: 2rem;
-  height: 2rem;
-  padding: 0;
+}
+*/
+
+.cms-new-adventure-fields-container {
+  padding: 2rem;
+}
+
+.cms-new-adventure-fields-container h2 {
+  margin-bottom: 2rem;
+}
+
+@media(min-width: 600px) {
+  .cms-new-adventure-fields {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+  }
+}
+
+.cms-new-adventure-fields label {
+  display: block;
+  margin-bottom: calc(6rem / 16);
+}
+
+@media(min-width: 600px) {
+  .cms-new-adventure-fields label {
+    margin: 0;
+  }
+}
+
+.cms-new-adventure-fields input {
+  width: 100%;
+  height: 2.5rem;
+  padding: 0.25rem 0.6rem;
+  font-size: 1em;
+  box-sizing: border-box;
+  margin-bottom: 1rem;
+}
+
+.cms-new-adventure-fields input:last-child {
+  margin-bottom: 0;
+}
+
+@media(min-width: 600px) {
+  .cms-new-adventure-fields input {
+    margin-bottom: 0;
+  }
+}
+
+.cms-new-adventure-actions {
+  display: flex;
+  justify-content: end;
+  gap: 0.5rem;
+  margin-top: 2rem;
+}
+
+.cms-new-adventure-actions button {
   border: 1px solid #787878;
-  border-radius: 50%;
-  background: #fff;
-  cursor: pointer;
+  border-radius: 8px;
+  background: none;
+  padding: 12px;
+  font-size: 1em;
+}
+
+.cms-new-adventure-actions button.button-ok {
+  background-color: rgba(0, 255, 21, 0.178);
+}
+
+.cms-new-adventure-actions button.button-ok:active {
+    background-color: rgb(0 145 12 / 18%);
+}
+
+.cms-new-adventure-actions button.button-cancel:active {
+    background-color: rgba(145, 0, 0, 0.18);
 }
 </style>
