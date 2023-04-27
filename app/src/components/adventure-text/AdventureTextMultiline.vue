a69e16ee3eb9ccaeefa7b76b25738194399cb010
Tom Ladek <ladek.tom@gmail.com>
2023-04-27 09:39:14 +0200

    fix changing display language not changing editor content add editor button container when editor is focused

--- app/src/components/adventure-text/AdventureTextMultiline.vue
+++ app/src/components/adventure-text/AdventureTextMultiline.vue
@@ -1,5 +1,5 @@
 <script>
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 /* CMS */
@@ -20,15 +20,12 @@ const props = defineProps({
   }
 });
 
-const { t } = useI18n();
+const { t, locale } = useI18n();
 
 const realTextDisplay = ref(true);
 
 const finalText = computed(() => {
-  if (!props.textModule)
-    return "";
-
-  let textTmp = t(props.textModule);
+  let textTmp = getTranslatedText();
   
   if (textTmp.length === 0)
     return "";
@@ -36,17 +33,38 @@ const finalText = computed(() => {
   return /<p>.*<\/p>/.test(textTmp) ? textTmp : `<p>${textTmp}</p>`;
 })
 
+function getTranslatedText() {
+  return props.textModule ? t(props.textModule) : "";
+}
+
 /* CMS */
+const editorControlsShown = ref(false),
+      cmsControlsPosition = ref({ top: 0, left: 0 });
+
 const editor = useEditor({
-  content: props.textModule ? t(props.textModule) : "",
+  content: getTranslatedText(),
   extensions: [ StarterKit ],
   onBeforeCreate() {
-    realTextDisplay.value = false
+    realTextDisplay.value = false;
   },
   onUpdate({ editor }) {
     // console.log(editor.getHTML())
+  },
+  onFocus() {
+    editorControlsShown.value = true
+  },
+  onBlur() {
+    editorControlsShown.value = false
+  },
+  onCreate({ editor }) {
+    cmsControlsPosition.value.top = `${editor.view.dom.offsetTop}px`;
+    cmsControlsPosition.value.left = `${editor.view.dom.offsetLeft}px`;
   }
 });
+
+watch(locale, () => {
+  editor.value.commands.setContent(getTranslatedText())
+})
 /* /CMS */
 </script>
 
@@ -54,7 +72,12 @@ const editor = useEditor({
   <div v-if="finalText" class="text-wrapper" v-html="finalText" v-show="realTextDisplay"></div>
 
   <!-- CMS -->
-  <EditorContent :editor="editor" class="cms-text-editor" />
+  <div class="cms-test-editor-container" v-show="!realTextDisplay">
+    <EditorContent :editor="editor" class="cms-text-editor" />
+    <div class="cms-text-editor-controls" v-show="editorControlsShown">
+      <button>✔️</button>
+    </div>
+  </div>
   <!-- /CMS -->
 </template>
 
@@ -71,6 +94,16 @@ const editor = useEditor({
 .cms-text-editor .ProseMirror:focus-visible {
   outline-style: double;
   outline-offset: -1px;
+}
+
+.cms-test-editor-container .cms-text-editor-controls {
+  position: fixed;
+  top: v-bind("cmsControlsPosition.top");
+  left: v-bind("cmsControlsPosition.left");
+  background: white;
+  color: black;
+  border-radius: 0.4rem;
+  transform: translateY(-100%);
 }
 /* /CMS */
 </style>
