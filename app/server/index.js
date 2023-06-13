97cc709d1b268dbc944565129b53f9ec874df90a
Tom Ladek <ladek.tom@gmail.com>
2023-06-13 11:36:47 +0200

    add winston request logging

--- app/server/index.js
+++ app/server/index.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const multer = require('multer')
+const winston = require('winston')
+const expressWinston = require('express-winston')
 const { renderPage } = require('vite-plugin-ssr/server')
 const { execSync } = require('child_process')
 
@@ -77,6 +79,17 @@ async function startServer() {
 
 
   /* Middlewares */
+  app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    meta: false, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) { return false } // optional: allows to skip some log messages based on request and/or response
+  }))
+
   app.use(express.json())
 
   if (isProduction) {
