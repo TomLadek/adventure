77c6bc3264f40325067dfc2fd7384b7057df5234
Tom Ladek <ladek.tom@gmail.com>
2023-08-25 10:54:19 +0200

    fix problem with assigning adventure when staging URL ended with a slash

--- app/pages/adventure/_default.page.server.js
+++ app/pages/adventure/_default.page.server.js
@@ -6,7 +6,7 @@ export { onBeforeRender }
 async function onBeforeRender(pageContext) {
   console.log(`onBeforeRender (adventure) -- ${pageContext.urlPathname} -- ${pageContext.Page ? pageContext.Page.__name : ""}`)
 
-  const adventureUrlPath = isCmsView ? (pageContext.urlPathname.replace(/^\//, "")) : process.env.DEPLOYMENT_PATH
+  const adventureUrlPath = isCmsView ? pageContext.urlPathname.replace(/^\/|\/$/g, "") : process.env.DEPLOYMENT_PATH
   let adventure = await findAdventure(adventureUrlPath) || {}
 
   return {
